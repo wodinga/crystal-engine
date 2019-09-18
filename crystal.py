e693ef7f94f0e93b5c9ad06011b0eebd0d1652fa
raf <1337technomage@gmail.com>
2019-09-18 17:50:18 -0400

    Playing midi file correctly

--- crystal.py
+++ crystal.py
@@ -20,44 +20,57 @@ class crystalengine:
         self.playback = self.client.get_ports(is_audio=True, is_output=False, is_physical=True)
         print(self.playback)
         self.midiOut = self.client.midi_outports.register('midiOut')
+        self.offset = 0
+        self.msg = next(self.aella)
+
         @self.client.set_samplerate_callback
         def samplerate(samplerate):
             self.sr = samplerate
+            print(self.sr)
 
         @self.client.set_shutdown_callback
         def shutdown(status, reason):
-            self.midiOut.clear_buffer()
             print('JACK shutdown:', reason, status)
+            event.set()
 
         @self.client.set_process_callback
         def process(frames):
+            midiOut = self.midiOut
             self.midiOut.clear_buffer()
-            offset = 0
-            for note in self.aella:
-                print(note)
+            while True:
+                if self.offset >= frames:
+                    self.offset -= frames
+                    return  # We'll take care of this in the next block ...
+                # Note: This may raise an exception:
+                self.midiOut.write_midi_event(self.offset, self.msg.bytes())
+                #print(msg)
                 try:
-                    self.midiOut.write_midi_event(offset, note.bytes())
-                except Exception as e:
-                    print(e)
+                    self.msg = next(self.aella)
+                except StopIteration:
+                    event.set()
                     raise jack.CallbackExit
-                offset += round(note.time * self.sr)
+                self.offset += round(self.msg.time * self.sr)
+
+
 
     def playSound(self):
         client = self.client
         synth = self.synth
         playback = self.playback
         with client:
-            self.midiOut.clear_buffer()
+            print(self.connect_to)
+            if self.connect_to:
+                self.midiOut.connect(self.connect_to)
             if len(client.get_all_connections(synth[0])) == 0:
                 client.connect(synth[0], playback[0])
                 client.connect(synth[1], playback[1])
                 print('should be playing')
-                try:
-                    event.wait()
-                except KeyboardInterrupt:
-                    print('\nInterrupted by user')
-            if self.connect_to:
-                self.midiOut.connect(self.connect_to)
+            print('Playing', repr(self.filename), '... press Ctrl+C to stop')
+            try:
+                event.wait()
+            except KeyboardInterrupt:
+                self.midiOut.clear_buffer()
+                print('\nInterrupted by user')
 
 engine = crystalengine()
 engine.playSound()
