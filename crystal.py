ec6348ba9d43550dca70a11f54b6f29135b4fcfb
Rafael Garcia <1337technomage@gmail.com>
2019-09-28 16:07:43 -0400

    Play audio files in unison

--- crystal.py
+++ crystal.py
@@ -5,14 +5,16 @@ import keyboard
 
 class crystalengine:
     def __init__(self):
-        self.PODIUM = {
-            'red': 'Untitled 1-Sylenth1.wav',
-            'blue': 'Untitled 2-Sylenth1.wav',
-            'green': 'Untitled 3-Sylenth1.wav',
-            'yellow': 'Untitled 4-Sylenth1.wav'
+        self.PODIUMS = {
+            'red': 'sounds/Untitled 1-Sylenth1.wav',
+            'blue': 'sounds/Untitled 2-Sylenth1.wav',
+            'green': 'sounds/Untitled 3-Sylenth1.wav',
+            'yellow': 'sounds/Untitled 4-Sylenth1.wav'
         }
-        self.active_podiums = dict.fromkeys(self.PODIUM, False)
+        self.active_podiums = dict.fromkeys(self.PODIUMS, False)
         self.tfm = sox.Transformer()
+        self.tfm.set_globals(verbosity=3) # for debugging
+        self.comb = sox.Combiner()
 
     def activatePodium(self, podium):
         if podium in self.active_podiums:
@@ -24,9 +26,24 @@ class crystalengine:
             self.active_podiums[podium] = False
             print(self.active_podiums)
 
+    def play_sound(self):
+        #Filter active_podiums to get only podiums set to True
+        active_dict = {k: v for k,v in self.active_podiums.items() if v==True}
+        #Get keys of active podiums
+        keys = active_dict.keys()
+        # Use keys to grab filenames
+        files = list(map(lambda x: self.PODIUMS[x], keys)) # Contains list of audio files for active podiums
+        # Grabs file extension and sets file_type
+        self.comb.set_input_format(file_type=list(map(lambda file: file.split('.')[1], files)))
+        print(active_dict)
+        print(keys)
+        for entry in files:
+            print(entry)
+        self.comb.preview(files, combine_type="merge")
+
 crystal = crystalengine()
 
 crystal.activatePodium('red')
 crystal.activatePodium('green')
-crystal.deactivatePodium('green')
 crystal.deactivatePodium('yellow')
+crystal.play_sound()
